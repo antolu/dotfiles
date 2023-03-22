0583708f10a26caa5d6116c3b1ea86ea72c50a21
Anton Lu <anton96lu@gmail.com>
2023-03-22 22:22:48 +0100

    cleanup and fix barbar keybindings

--- nvim/lua/plugins/configs/barbar.lua
+++ nvim/lua/plugins/configs/barbar.lua
@@ -3,46 +3,6 @@ local M = {}
 local barbar = require("bufferline")
 local wk = require("which-key")
 
-local map = vim.api.nvim_set_keymap
-local opts = { noremap = true, silent = true }
-
--- Move to previous/next
-map("n", "<A-,>", "<Cmd>BufferPrevious<CR>", opts)
-map("n", "<A-.>", "<Cmd>BufferNext<CR>", opts)
--- Re-order to previous/next
-map("n", "<A-<>", "<Cmd>BufferMovePrevious<CR>", opts)
-map("n", "<A->>", "<Cmd>BufferMoveNext<CR>", opts)
--- Goto buffer in position...
-map("n", "<A-1>", "<Cmd>BufferGoto 1<CR>", opts)
-map("n", "<A-2>", "<Cmd>BufferGoto 2<CR>", opts)
-map("n", "<A-3>", "<Cmd>BufferGoto 3<CR>", opts)
-map("n", "<A-4>", "<Cmd>BufferGoto 4<CR>", opts)
-map("n", "<A-5>", "<Cmd>BufferGoto 5<CR>", opts)
-map("n", "<A-6>", "<Cmd>BufferGoto 6<CR>", opts)
-map("n", "<A-7>", "<Cmd>BufferGoto 7<CR>", opts)
-map("n", "<A-8>", "<Cmd>BufferGoto 8<CR>", opts)
-map("n", "<A-9>", "<Cmd>BufferGoto 9<CR>", opts)
-map("n", "<A-0>", "<Cmd>BufferLast<CR>", opts)
--- Pin/unpin buffer
-map("n", "<A-p>", "<Cmd>BufferPin<CR>", opts)
--- Close buffer
-map("n", "<A-c>", "<Cmd>BufferClose<CR>", opts)
--- Wipeout buffer
---                 :BufferWipeout
--- Close commands
---                 :BufferCloseAllButCurrent
---                 :BufferCloseAllButPinned
---                 :BufferCloseAllButCurrentOrPinned
---                 :BufferCloseBuffersLeft
---                 :BufferCloseBuffersRight
--- Magic buffer-picking mode
-map("n", "<C-p>", "<Cmd>BufferPick<CR>", opts)
--- Sort automatically by...
-map("n", "<Space>bb", "<Cmd>BufferOrderByBufferNumber<CR>", opts)
-map("n", "<Space>bd", "<Cmd>BufferOrderByDirectory<CR>", opts)
-map("n", "<Space>bl", "<Cmd>BufferOrderByLanguage<CR>", opts)
-map("n", "<Space>bw", "<Cmd>BufferOrderByWindowNumber<CR>", opts)
-
 local function set_keymaps(bufnr)
 	wk.register({
 		b = {
@@ -63,6 +23,14 @@ local function set_keymaps(bufnr)
 			["0"] = { "<Cmd>BufferLast <CR>", "Go to last buffer" },
 			["p"] = { "<Cmd>BufferPin<CR>", "Pin buffer" },
 			["c"] = { "<Cmd>BufferClose<CR>", "Close buffer" },
+            ["b"] = { "<Cmd>BufferPick<CR>", "Pick buffer" },
+		},
+	},
+    {
+        prefix = "<leader>",
+        buffer = bufnr,
+    })
+    wk.register({
 			s = {
 				name = "Sort",
 				["b"] = { "<Cmd>BufferOrderByBufferNumber<CR>", "Sort by buffer number" },
@@ -70,12 +38,14 @@ local function set_keymaps(bufnr)
 				["l"] = { "<Cmd>BufferOrderByLanguage<CR>", "Sort by language" },
 				["w"] = { "<Cmd>BufferOrderByWindowNumber<CR>", "Sort by window number" },
 			},
-		},
-		{
-			prefix = "<leader>",
-			buffer = bufnr,
-		},
-	})
+    {
+        prefix = "<leader>b",
+        buffer = bufnr,
+    }
+    })
+    wk.register({
+        ["b"] = { "<Cmd>BufferPick<CR>", "Pick buffer" },
+    })
 end
 
 function M.setup()
@@ -83,7 +53,7 @@ function M.setup()
 		animation = true,
 		auto_hide = true,
 		tabpages = true,
-		closble = true,
+		closable = true,
 		clickable = true,
 		diagnostics = {
 			-- you can use a list
@@ -98,7 +68,7 @@ function M.setup()
 			[vim.diagnostic.severity.HINT] = { enabled = true },
 		},
 		focus_on_close = "left",
-		hide = { inactive = true },
+--		hide = { inactive = true },
 		highlight_alternate = false,
 		highlight_inactive_file_icons = false,
 		highlight_visible = true,
@@ -115,6 +85,7 @@ function M.setup()
 		minimum_padding = 1,
 		maximum_length = 30,
 		semantic_letters = true,
+        letters = 'asdfjkl;ghnmxcvbziowerutyqpASDFJKLGHNMXCVBZIOWERUTYQP',
 		no_name_title = nil,
 	})
 	set_keymaps()
