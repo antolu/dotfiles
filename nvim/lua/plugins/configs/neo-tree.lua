896b4e18b7d8efae3d82ab9df863462aae3992d3
Anton Lu <anton96lu@gmail.com>
2023-03-27 21:45:53 +0200

    add neo-tree.lua

---
+++ nvim/lua/plugins/configs/neo-tree.lua
@@ -0,0 +1,27 @@
+local M = {}
+
+local neo_tree = require('neo-tree')
+local wk = require('which-key')
+
+function set_keymaps()
+    wk.register({
+        n = {
+            name = 'NeoTree',
+            f = { "<Cmd>Neotree reveal filesystem left<CR>", "Show files" },
+            F = { "<Cmd>Neotree toggle filesystem left<CR>", "Toggle files" },
+            b = { "<Cmd>Neotree reveal buffers right<CR>", "Show buffers" },
+            B = { "<Cmd>Neotree toggle buffers right<CR>", "Toggle buffers" },
+            g = { "<Cmd>Neotree show focus git_status float<CR>", "Show git" },
+        },
+    }, {
+        prefix = '<leader>',
+    })
+end
+
+function M.setup()
+    set_keymaps()
+
+    neo_tree.setup({})
+end
+
+return M
