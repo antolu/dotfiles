b06da65e5a774d8ffdfe6aefd46626e8f3e59c73
Anton Lu <anton96lu@gmail.com>
2023-09-19 11:27:53 +0200

    update null ls

--- dot_config/nvim/lua/plugins/configs/null-ls.lua
+++ dot_config/nvim/lua/plugins/configs/null-ls.lua
@@ -4,28 +4,28 @@ local null_ls = require("null-ls")
 local lsp_config = require("plugins.configs.lsp")
 
 function M.setup()
-	local code_actions = null_ls.builtins.code_actions
-	local diagnostics = null_ls.builtins.diagnostics
-	local formatting = null_ls.builtins.formatting
-	local completion = null_ls.builtins.completion
+    local code_actions = null_ls.builtins.code_actions
+    local diagnostics = null_ls.builtins.diagnostics
+    local formatting = null_ls.builtins.formatting
+    local completion = null_ls.builtins.completion
 
-	null_ls.setup({
-		sources = {
-			code_actions.refactoring,
-			diagnostics.codespell,
-			-- diagnostics.flake8,
-			diagnostics.mypy,
-			--completion.tags,
-			formatting.black,
-			formatting.isort,
-			formatting.ruff,
-			formatting.stylua,
-			-- diagnostics.eslint_d,
-			-- code_actions.eslint_d,
-			-- formatting.prettierd,
-		},
-		on_attach = lsp_config.on_attach_no_symbols,
-	})
+    null_ls.setup({
+        sources = {
+            code_actions.refactoring,
+            diagnostics.codespell,
+            -- diagnostics.flake8,
+            diagnostics.mypy,
+            --completion.tags,
+            formatting.black,
+            formatting.isort,
+            formatting.ruff,
+            formatting.stylua,
+            -- diagnostics.eslint_d,
+            -- code_actions.eslint_d,
+            -- formatting.prettierd,
+        },
+        on_attach = lsp_config.on_attach_no_symbols,
+    })
 end
 
 return M
