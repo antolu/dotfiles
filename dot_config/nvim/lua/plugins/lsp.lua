8f845d481d9696cae645a7b04c1856eecc0bad74
Anton Lu <anton96lu@gmail.com>
2025-08-27 15:01:05 +0200

    update neovim

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -1,16 +1,30 @@
 return {
-  "neovim/nvim-lspconfig",
-  opts = {
-    servers = {
-      basedpyright = {
-        settings = {
-          basedpyright = {
-            analysis = {
-              typeCheckingMode = "off",
-            },
-          },
-        },
-      },
-    },
-  },
+	{
+		"neovim/nvim-lspconfig",
+		opts = {
+			servers = {
+				basedpyright = {
+					settings = {
+						python = {
+							analysis = {
+								ignore = { "*" },
+								typeCheckingMode = "off",
+								reportUnknownParameterType = "off",
+							},
+						},
+					},
+				},
+				ruff = {
+					settings = {},
+				},
+			},
+		},
+	},
+	{
+		"nvimtools/none-ls.nvim",
+		opts = function(_, opts)
+			local nls = require("null-ls")
+			opts.sources = vim.list_extend(opts.sources, { nls.builtins.formatting.mypy })
+		end,
+	},
 }
